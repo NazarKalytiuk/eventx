96139fce9452331ba1d3e8d687733c0a945edd72
Nazar Kalytiuk <nazarkalytiuk@gmail.com>
2018-02-24 16:43:50 +0200

    Add firebase deploy
    Signed-off-by: Nazar Kalytiuk <nazarkalytiuk@gmail.com>

--- .circleci/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL"
-git config --global user.name "$GH_NAME"
-
-npm run deploy.github
-
-echo "Finished Deployment!"
